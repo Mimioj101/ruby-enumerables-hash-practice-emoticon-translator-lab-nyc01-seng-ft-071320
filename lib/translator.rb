e24198023a29e63626a0c80bf08da1ca4f58b8bd
Mimi Ojserkis <mimioj@gmail.com>
2020-07-08 20:45:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,8 +10,8 @@ def load_library(file_path)
  emoticons_hash = {}
      emo.each do |name,arrays|
       emoticons_hash[name] ||= {}
-      frank_emo[name][:english] = arrays[0]
-      frank_emo[name][:japanese] = arrays[1]
+      emoticons_hash[name][:english] = arrays[0]
+      emoticons_hash[name][:japanese] = arrays[1]
   end
 frank_emo
   end
