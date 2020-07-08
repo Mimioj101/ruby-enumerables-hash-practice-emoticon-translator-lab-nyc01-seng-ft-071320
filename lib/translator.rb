f031cd94e13d859ebbb5d8adf7603c7e73ac1060
Mimi Ojserkis <mimioj@gmail.com>
2020-07-08 20:22:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,12 +1,24 @@
-# require modules here
 
-def load_library
-  # code goes here
+
+
+
+
+
+def load_library(file)
+
 end
+
+
+
+
 
 def get_japanese_emoticon
   # code goes here
 end
+
+
+
+
 
 def get_english_meaning
   # code goes here
